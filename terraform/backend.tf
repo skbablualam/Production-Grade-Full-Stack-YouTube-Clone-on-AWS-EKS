4f598822c94c81257a323d8610fa1eb3af5f1639
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2026-02-23 15:30:42 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bablu-prod-terraform-state"
-    key            = "prod-app/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
