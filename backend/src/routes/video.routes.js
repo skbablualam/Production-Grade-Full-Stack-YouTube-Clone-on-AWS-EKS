eae832699766b11e98360907baaf28c197b908f0
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2026-02-23 15:12:28 +0530

    Add CRUD routes for video management

--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -4,5 +4,8 @@ const controller = require('../controllers/video.controller');
 
 router.post('/', controller.createVideo);
 router.get('/', controller.getVideos);
+router.get('/:id', controller.getVideoById);
+router.put('/:id', controller.updateVideo);
+router.delete('/:id', controller.deleteVideo);
 
 module.exports = router;
