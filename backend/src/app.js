e59ddc403112c90deb09d9279512af11e5e97b4a
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2026-02-23 15:09:13 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const cors = require('cors');
 
+const userRoutes = require('./routes/user.routes');
+const videoRoutes = require('./routes/video.routes');
+const errorMiddleware = require('./middlewares/error.middleware');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/videos', videoRoutes);
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
+
+app.use(errorMiddleware);
+
+module.exports = app;
