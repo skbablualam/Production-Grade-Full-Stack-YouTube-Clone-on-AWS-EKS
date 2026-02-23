83bcbbe9238d32d4b5d4fea675501c32a8ec60d5
Bablu Alam <52425424+skbablualam@users.noreply.github.com>
2026-02-23 15:07:47 +0530

    Add user routes for creating and retrieving users

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/user.controller');
 
+router.post('/', controller.createUser);
+router.get('/', controller.getUsers);
+
+module.exports = router;
